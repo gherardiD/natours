b3d70b21f5ce92dc6990f8223adc2bac7ad4e138
gherardiD <gherardi.daniele.studente@itispaleocapa.it>
2024-07-25 13:13:02 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    // ! Deprecation parameters
+    // useNewUrlParser defines how the connection string is parsed
+    useNewUrlParser: true,
+    // useCreateIndex ensures that when there is a new index, it is created
+    useCreateIndex: true,
+    // useFindAndModify ensures that the findAndModify is used for all queries
+    useFindAndModify: false,
+    // useUnifiedTopology ensures that the server discovery and monitoring engine is used
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
